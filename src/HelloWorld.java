02603cf2f86c21055e5e38b60fba63877717ab96
student <student@158.197.35.80>
2018-09-19 09:23:07 +0200

    asd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,16 +7,13 @@ public class HelloWorld {
 			System.out.println("this sucks big time");
 		}
 		System.out.println("And you suck even more!");
-<<<<<<< HEAD
 
 		System.out.println("toto je spravny kod!");
-=======
-		//asd
+	
+	//asd
 		//ta ty riadne supacis 
 		//testovanie pushu
 		
-		
-		//rip testovanie projektu
->>>>>>> branch 'master' of https://github.com/MatusLach/paz1cCvicenia
+	
 	}
 }

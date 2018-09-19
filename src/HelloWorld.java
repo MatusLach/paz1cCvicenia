f190585ec715f857d5aa46641723b26560740322
student <student@158.197.35.80>
2018-09-19 08:04:30 +0200

    added sysout

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		for (int i = 0; i < 10; i++) {
-			System.out.println("this sucks");
+			System.out.println("this sucks big time");
 		}
+		System.out.println("And you suck even more!");
 	}
 }
